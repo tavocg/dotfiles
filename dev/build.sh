ad61fe749f8986d7780ee282eb3ae6bf21150d00
tavo <tavo@tavo.one>
2024-11-10 21:18:51 -0600

    err manage

--- dev/build.sh
+++ dev/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+SSH_KEY_ARG="$1"
+[ -z "$SSH_KEY_ARG" ] && echo "No ssh-key provided" && exit 1
+
 podman rm -f dev
-podman build --build-arg SSH_KEY="$(cat ~/.ssh/id_ed25519.pub)" -t debian-dev-env .
+podman build --build-arg SSH_KEY="$SSH_KEY_ARG" -t debian-dev-env .
