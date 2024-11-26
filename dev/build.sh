49c39fa36cea1a818dd34292110b21643d288954
tavo <tavo@tavo.one>
2024-11-26 16:26:14 -0600

    typo

--- dev/build.sh
+++ dev/build.sh
@@ -2,7 +2,7 @@
 
 FJP_ARG="$1"
 [ -z "$FJP_ARG" ] && FJP_ARG="$(pass personal/forgejo | sed '/public-keys/!d;s/^.*: //')"
-[ -z "$FJP_ARG" ] && echo "No ssh-key provided" && exit 1
+[ -z "$FJP_ARG" ] && echo "No forgejo token provided" && exit 1
 
 podman rm -f dev
 podman build --build-arg FJP="$FJP_ARG" -t arch-dev-env .
