26033cfe7877295dc50421d861dd9aff4a22a4a1
tavo <tavo@tavo.one>
2024-11-26 20:24:41 -0600

    dumb

---
+++ dev/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "HOSTNAME=''" >> ~/.bash_profile
+echo ". ~/.config/shell/env-min" >> ~/.bash_profile
+echo ". ~/.config/shell/bashrc" >> ~/.bashrc
+
+sudo pacman -S --needed --noconfirm sudo man-db exa curl jq ffmpeg imagemagick \
+    nnn screen tmux groff openssh base-devel git neovim ripgrep fzf emacs \
+    python python-pip python-virtualenv go go-tools clang nodejs npm php cargo \
+    texlive-core texlive-fontsextra r
+
+git clone git@git.tavo.one:tavo/dotfiles.git ~/.config
+
+sudo pacman -S --needed git base-devel
+sudo mkdir -p /opt/yay
+sudo chown -R dev:dev /opt/yay
+git clone https://aur.archlinux.org/yay.git /opt/yay
+(cd /opt/yay && makepkg -si --noconfirm)
+
+LV_BRANCH='release-1.4/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.4/neovim-0.9/utils/installer/install.sh)
