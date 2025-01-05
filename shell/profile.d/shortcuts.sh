951ef339ff0400ab041202c068c43d4b4cc466db
tavo <tavo@tavo.one>
2025-01-05 11:35:45 -0600

    cleanup

--- shell/profile.d/shortcuts.sh
+++ shell/profile.d/shortcuts.sh
@@ -1,7 +1,7 @@
 export \
-    GIT="git@git.tavo.one:tavo" \
-    GITLAB="git@gitlab.com:tavo-wasd" \
-    GITHUB="git@github.com:tavo-wasd-gh" \
+    FJ="git@git.tavo.one:tavo" \
+    GL="git@gitlab.com:tavo-wasd" \
+    GH="git@github.com:tavo-wasd-gh" \
     DRIVE="ssh://drive:/home/drive/drive" \
     BOOKMARKS="$HOME/Documents/bookmarks" \
     BIB="$HOME/Documents/bibliography" \
