8b10cb3963396ff243880436f6e4b9a07b6400c4
tavo <tavo@tavo.one>
2025-08-03 18:07:37 -0600

    yazi wrapper

---
+++ shell/profile.d/node.sh
@@ -0,0 +1,6 @@
+export NODE_REPL_HISTORY="$HOME"/.local/state/node_repl_history
+
+if ! [ -d "${NODE_REPL_HISTORY%/*}" ] ; then
+    mkdir -p "${NODE_REPL_HISTORY%/*}"
+    touch "$NODE_REPL_HISTORY"
+fi
--- shell/profile.d/xdgspec.sh
+++ shell/profile.d/xdgspec.sh
@@ -28,6 +28,10 @@ export \
     SSH_CONFIG="-F ${XDG_CONFIG_HOME}/ssh/config" \
     GIT_SSH_COMMAND="ssh -F ${XDG_CONFIG_HOME}/ssh/config" \
     ZDOTDIR="$XDG_CONFIG_HOME/shell" \
-    NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/npmrc \
+    NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc" \
     IPYTHONDIR="$XDG_CONFIG_HOME/ipython" \
-    PYENV_ROOT="$XDG_DATA_HOME"/pyenv
+    PYENV_ROOT="$XDG_DATA_HOME/pyenv" \
+    PYTHON_HISTORY="$XDG_CACHE_HOME/python_history" \
+    SQLITE_HISTORY="$XDG_CACHE_HOME/sqlite_history" \
+    PGPASSFILE="$XDG_CONFIG_HOME/pg/pgpass" \
+    XDG_DATA_DIRS="$XDG_DATA_HOME/flatpak/exports/share${XDG_DATA_DIRS:+:${XDG_DATA_DIRS}}"
