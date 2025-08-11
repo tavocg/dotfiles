5806239856d9caef0177b98ef3b5f239b95ee783
tavo <tavo@tavo.one>
2025-08-11 00:22:57 -0600

    updates

--- shell/profile.d/config.sh
+++ shell/profile.d/config.sh
@@ -1,5 +1,5 @@
-export GTK_THEME="Adwaita-dark"
-export XCURSOR_THEME="Adwaita"
+#export GTK_THEME="Adwaita-dark"
+#export XCURSOR_THEME="Adwaita"
 #export QT_QPA_PLATFORMTHEME="qt5ct"
 export CM_SELECTIONS="clipboard"
 export CM_MAX_CLIPS=10
