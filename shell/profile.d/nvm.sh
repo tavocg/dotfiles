ad4e66377071301ef6097d62b2fbebee876d858e
tavo <tavo@tavo.one>
2025-09-04 21:07:11 -0600

    shellcheck and faster startup

--- shell/profile.d/nvm.sh
+++ shell/profile.d/nvm.sh
@@ -3,5 +3,10 @@
 export NVM_DIR="$HOME/.config/nvm"
 
 if [ -s "$NVM_DIR/nvm.sh" ]; then
-  . "$NVM_DIR/nvm.sh"
+  # nvm.sh is a very large file that causes shell startup to slow
+  # down, this wrapper sources nvm.sh only after calling nvm.
+  nvm() {
+    . "$NVM_DIR/nvm.sh"
+    nvm "$@"
+  }
 fi
--- shell/profile.d/shortcuts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export \
-  FJ="git@git.tavo.one:tavo" \
-  GL="git@gitlab.com:tavo-wasd" \
-  GH="git@github.com:tavo-wasd-gh" \
-  DRIVE="ssh://drive:/home/drive/drive" \
-  BOOKMARKS="$HOME/Documents/bookmarks" \
-  BIB="$HOME/Documents/bibliography"
