a6433634d69c3091c583073bfa03c09eb09dcb1a
tavo <tavo@tavo.one>
2025-12-18 08:07:48 -0600

    new users

--- shell/profile.d/shortcuts.sh
+++ shell/profile.d/shortcuts.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 export \
-  FJ="git@git.tavo.one:tavo" \
-  GL="git@gitlab.com:tavo-wasd" \
-  GH="git@github.com:tavo-wasd-gh" \
+  GL="git@gitlab.com:tavocg" \
+  GH="git@github.com:tavocg" \
   DRIVE="ssh://drive:/home/drive/drive" \
   BOOKMARKS="$HOME/Documents/bookmarks" \
   BIB="$HOME/Documents/bibliography"
