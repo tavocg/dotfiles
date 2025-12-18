f4964a5f1f32bf0485599e0b5129dac554c67ba9
tavo <tavo@tavo.one>
2025-12-18 08:07:39 -0600

    nvm

--- shell/profile.d/nvm.sh
+++ shell/profile.d/nvm.sh
@@ -2,16 +2,42 @@
 
 export NVM_DIR="$HOME/.config/nvm"
 
-_nvm_lazy_load() {
+_nvm_cd_callback() {
   if ! [ -f "package-lock.json" ] && ! [ -f "package.json" ] && ! [ -d "node_modules" ]; then
     return
   fi
 
-  if command -v nvm >/dev/null 2>&1; then
-    return
+  [ -n "$_NVM_LOADED" ] && return
+
+  if [ -s "$NVM_DIR/nvm.sh" ]; then
+    _NVM_LOADED=1
+    . "$NVM_DIR/nvm.sh"
   fi
+}
+
+_nvm_lazy_load() {
+  [ -n "$_NVM_LOADED" ] && return
+  _NVM_LOADED=1
 
   if [ -s "$NVM_DIR/nvm.sh" ]; then
     . "$NVM_DIR/nvm.sh"
   fi
+}
+
+nvm() {
+  unset -f nvm
+  _nvm_lazy_load
+  command nvm "$@"
+}
+
+node() {
+  unset -f node
+  _nvm_lazy_load
+  command node "$@"
+}
+
+npm() {
+  unset -f npm
+  _nvm_lazy_load
+  command npm "$@"
 }
