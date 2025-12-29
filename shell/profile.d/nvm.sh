fcdb1c6844ac717d66b09e33e00fd1493487fa62
tavo <tavo@tavo.one>
2025-12-29 11:32:34 -0600

    update

--- shell/profile.d/nvm.sh
+++ shell/profile.d/nvm.sh
@@ -17,27 +17,24 @@ _nvm_cd_callback() {
 
 _nvm_lazy_load() {
   [ -n "$_NVM_LOADED" ] && return
-  _NVM_LOADED=1
 
   if [ -s "$NVM_DIR/nvm.sh" ]; then
     . "$NVM_DIR/nvm.sh"
+    _NVM_LOADED=1
   fi
 }
 
 nvm() {
-  unset -f nvm
   _nvm_lazy_load
   command nvm "$@"
 }
 
 node() {
-  unset -f node
   _nvm_lazy_load
   command node "$@"
 }
 
 npm() {
-  unset -f npm
   _nvm_lazy_load
   command npm "$@"
 }
