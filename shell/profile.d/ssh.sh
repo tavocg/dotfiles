5dbeb9e61db6e1fbca656566d9edc302baccc344
tavo <gustavo.calvo.cr@gmail.com>
2026-01-12 19:30:55 -0600

    move explanation

--- shell/profile.d/ssh.sh
+++ shell/profile.d/ssh.sh
@@ -1,10 +1,4 @@
-# Use XDG spec with ssh, ultimately gave up on this beacause i need SFTP clients to work,
-# some allow to configure custom private key locations but other don't.
-
-# Set to something like 1 to enable
-USE_XDG_SSH_DIR=
-
-if [ "$USE_XDG_SSH_DIR" ] && [ -d "$XDG_CONFIG_HOME" ]; then
+if [ -e "$XDG_CONFIG_HOME"/ssh/config ]; then
   export SSH_CONFIG="-F ${XDG_CONFIG_HOME}/ssh/config"
 
   export GIT_SSH_COMMAND="ssh ${SSH_CONFIG}"
