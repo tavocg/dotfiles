f789cf05415ec1e10529ccf79f983842412886b7
tavocg <gustavo.calvo.cr@gmail.com>
2026-02-26 23:51:38 -0600

    ruby

---
+++ shell/profile.d/ruby.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+GEM_BASE="$HOME/.local/share/gem/ruby"
+latest_bin=""
+latest_home=""
+
+for d in "$GEM_BASE"/*/bin; do
+  [ -d "$d" ] || continue
+  latest_bin="$d"
+  latest_home=${d%/bin}
+done
+
+if [ -n "$latest_bin" ]; then
+  export GEM_HOME="$latest_home"
+  export PATH="$latest_bin:$PATH"
+fi
