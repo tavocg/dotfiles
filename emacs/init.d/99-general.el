5a610d87c59927d3a662f6ad6d9927bf1253de89
tavocg <gustavo.calvo.cr@gmail.com>
2026-03-24 21:00:14 -0600

    keys

--- emacs/init.d/99-general.el
+++ emacs/init.d/99-general.el
@@ -1,5 +1,5 @@
 (use-package general
-  :after (evil org-roam)
+  :after evil
   :demand t
   :config
   (general-evil-setup)
@@ -17,6 +17,17 @@
     "bn" '(next-buffer :wk "Next buffer")
     "bp" '(previous-buffer :wk "Previous buffer")
     "ff" '(find-file :wk "Find file")
+    "fs" '(save-buffer :wk "Save buffer"))
+
+  ;; Help
+  (my/leader-keys
+    "h" '(:ignore t :wk "Help")
+    "hf" '(describe-function :wk "Describe function")
+    "hv" '(describe-variable :wk "Describe variable")
+    )
+
+  ;; Org-Roam
+  (my/leader-keys
     "n" '(:ignore t :wk "notes")
     "nl" '(org-roam-buffer-toggle :wk "Roam buffer toggle")
     "nf" '(org-roam-node-find :wk "Find node")
@@ -24,4 +35,10 @@
     "ni" '(org-roam-node-insert :wk "Insert node")
     "nc" '(org-roam-capture :wk "Capture")
     "nj" '(org-roam-dailies-capture-today :wk "Daily journal")
-    ))
+    )
+
+  ;; Config
+  (my/leader-keys
+    "c" '(:ignore t :wk "config")
+    "cr" '((lambda () (interactive) (load-file user-init-file)) :wk "Reload config"))
+  )
---
+++ emacs/init.d/99-which-key.el
@@ -0,0 +1,4 @@
+(use-package which-key
+  :ensure nil
+  :config
+  (which-key-mode))
