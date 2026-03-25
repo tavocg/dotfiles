8822518dee91e19632d25672f93af94fc89cd48f
tavocg <gustavo.calvo.cr@gmail.com>
2026-03-25 13:33:43 -0600

    fix and other lsp

--- emacs/init.d/50-treesit.el
+++ emacs/init.d/50-treesit.el
@@ -5,10 +5,17 @@
 
 ;;; Code:
 
+(defvar treesit-language-source-alist nil)
+
 (use-package treesit-auto
   :functions (global-treesit-auto-mode treesit-auto-add-to-auto-mode-alist)
   :custom
   (treesit-auto-install 'prompt)
+  :init
+  ;; Emacs 30.1 on this system rejects the current upstream C grammar with ABI
+  ;; version 15, so pin C to a compatible release.
+  (setf (alist-get 'c treesit-language-source-alist)
+        '("https://github.com/tree-sitter/tree-sitter-c" "v0.23.6"))
   :config
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
