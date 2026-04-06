004b5ded4baf18da7bc7606a322443b1b2e3c624
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-06 09:16:16 -0600

    meh

---
+++ nix/flakes/packages/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "User packages";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.default =
+      let pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in pkgs.buildEnv {
+        name = "my-packages";
+        paths = with pkgs; [
+          git
+          htop
+          neovim
+        ];
+      };
+  };
+}
