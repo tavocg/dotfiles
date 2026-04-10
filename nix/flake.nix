bc6c27a41de74ae72c116c55c390d5872903d24f
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-10 00:31:37 -0600

    hmm

--- nix/flake.nix
+++
@@ -1,9 +0,0 @@
-{
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    import-tree.url = "github:vic/import-tree";
-  };
-
-  outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
-}
