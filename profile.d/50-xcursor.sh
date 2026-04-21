89eeb86b768e52b7e4139eae66c2c53a30fac638
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-21 05:17:26 -0600

    reorder

--- profile.d/50-xcursor.sh
+++ profile.d/50-xcursor.sh
@@ -1,11 +1,6 @@
 # shellcheck shell=sh
 
 case ":$XCURSOR_PATH:" in
-*":${XDG_DATA_HOME:-$HOME/.local/share}/icons:"*) ;;
-*) export XCURSOR_PATH="${XDG_DATA_HOME:-$HOME/.local/share}/icons${XCURSOR_PATH:+:${XCURSOR_PATH}}" ;;
-esac
-
-case ":$XCURSOR_PATH:" in
 *":/run/current-system/sw/share/icons:"*) ;;
 *) export XCURSOR_PATH="/run/current-system/sw/share/icons${XCURSOR_PATH:+:${XCURSOR_PATH}}" ;;
 esac
@@ -13,4 +8,9 @@ esac
 case ":$XCURSOR_PATH:" in
 *":/nix/var/nix/profiles/default/share/icons:"*) ;;
 *) export XCURSOR_PATH="/nix/var/nix/profiles/default/share/icons${XCURSOR_PATH:+:${XCURSOR_PATH}}" ;;
+esac
+
+case ":$XCURSOR_PATH:" in
+*":${XDG_DATA_HOME:-$HOME/.local/share}/icons:"*) ;;
+*) export XCURSOR_PATH="${XDG_DATA_HOME:-$HOME/.local/share}/icons${XCURSOR_PATH:+:${XCURSOR_PATH}}" ;;
 esac
