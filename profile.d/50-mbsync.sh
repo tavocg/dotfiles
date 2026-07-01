0573591ca5e3b739129bb89483de3a5bc2d34a90
tavocg <gustavo.calvo.cr@gmail.com>
2026-07-01 17:14:20 -0600

    create

--- profile.d/50-mbsync.sh
+++ profile.d/50-mbsync.sh
@@ -2,9 +2,11 @@
 
 export MBSYNC_MAILDIR="${MBSYNC_MAILDIR-$HOME/.local/share/isync}"
 
-if ! [ -d "$MBSYNC_MAILDIR" ]; then
-  mkdir -p "$MBSYNC_MAILDIR"
-fi
+for maildir in "$MBSYNC_MAILDIR" "$MBSYNC_MAILDIR"/personal "$MBSYNC_MAILDIR"/ucr; do
+  if ! [ -d "$maildir" ]; then
+    mkdir -p "$maildir"
+  fi
+done
 
 export MBSYNCRC="${XDG_CONFIG_HOME:-$HOME/.config}/isync/mbsyncrc"
 alias mbsync="mbsync -c "$XDG_CONFIG_HOME"/isync/mbsyncrc"
