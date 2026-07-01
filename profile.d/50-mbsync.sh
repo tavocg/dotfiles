a53576fe99eabbe219a153fd80c6383ed486963b
tavocg <gustavo.calvo.cr@gmail.com>
2026-07-01 17:12:47 -0600

    create

--- profile.d/50-mbsync.sh
+++ profile.d/50-mbsync.sh
@@ -1,4 +1,10 @@
 # shellcheck shell=sh
 
+export MBSYNC_MAILDIR="${MBSYNC_MAILDIR-$HOME/.local/share/isync}"
+
+if ! [ -d "$MBSYNC_MAILDIR" ]; then
+  mkdir -p "$MBSYNC_MAILDIR"
+fi
+
 export MBSYNCRC="${XDG_CONFIG_HOME:-$HOME/.config}/isync/mbsyncrc"
 alias mbsync="mbsync -c "$XDG_CONFIG_HOME"/isync/mbsyncrc"
